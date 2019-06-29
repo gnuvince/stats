a39931f1cc24e5ca43d84e5d99645441f9b7fa1d
Vincent Foley <vfoley@gmail.com>
2019-06-29 15:06:38 -0400

    Prepare 0.1.1 release

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::fs::File;
 use std::process::exit;
 
 const PROGNAME: &str = "stats";
-const VERSION: &str = "0.1.0";
+const VERSION: &str = "0.1.1";
 
 #[derive(Default, Debug)]
 struct Stats {
