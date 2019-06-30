fda516639d1b5debe0dfd3da10923d209f7c836c
Vincent Foley <vfoley@gmail.com>
2019-06-30 12:14:33 -0400

    Release stats 0.1.3

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::fs::File;
 use std::process::exit;
 
 const PROGNAME: &str = "stats";
-const VERSION: &str = "0.1.2";
+const VERSION: &str = "0.1.3";
 
 #[derive(Default, Debug)]
 struct Stats {
